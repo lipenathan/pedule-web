80fa2ee1843715c6b867940ca196940f087c8cd1
charlesYS <charlesmendes6@gmail.com>
2022-10-06 18:21:34 -0300

    login page created and almost finished

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,39 +1,64 @@
 <template>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-  <body>
+  <div class="body">
     <header>
             <img
-        id="logo"
-        alt="Logo Pedule"
-        src="../../../../Workspace/pedule-web/public/img/pedule-logo.png" />
+                id="logo"
+                alt="Logo Pedule"
+                src="../../../../Workspace/pedule-web/public/img/pedule-logo.png" />
 
-        <button class="cadastrar">Cadastre-se</button>
-     </header>
+                <button type="submit" class="btn-cadastrar" name="btn-cadastrar" @click="register"><a href="#">Cadastre-se</a></button>
+        </header>
     <main class="container">
-            <form action="" method="">
+
+
+            <form class="formulario" action="#" method="POST">
                 <fieldset class="card-login">
-                    <h2>Bem Vindo de volta</h2>
+                    <h1>Login</h1>
 
                     <label  for="email">E-mail</label>
-                    <input  id="email" type="text" placeholder="Insira o seu e-mail" autocomplete="off">
+                    <input  id="email" type="text" autofocus required v-model="form.email">
 
                     <label for="senha">Senha</label>
-                    <input type="password" name="senha" placeholder="Insira sua senha">
+                    <input id="senha" type="password" name="senha" required v-model="form.senha">
 
-                    <button type="submit" name="btn-entrar">Entre</button>
-                    <a href="#">Esqueceu sua senha?</a>
+                    <button type="submit" name="btn-entrar" @click="login"><a href="#">Entrar</a></button>
+                    <a href="#"><small>Esqueceu sua senha?</small></a>
                 </fieldset>
              </form> 
-        </main>
+    </main>
+  </div>
     
-   </body>
+    
+   
 </template>
 
 <script>
-
+//import {required, minLenght, email} from "vuelidate/lib/validators"
+export default {
+    data() {
+        return {
+            form: {
+                email: "",
+                senha: ""
+            }
+        }
+    },
+    methods() {
+        login(),
+        register()
+    }
+}
 </script>
 
 <style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap');
+
+    
+$background: rgb(255, 255, 254);
+$button: rgb(61, 169, 252);
+$tittle: rgb(9, 64, 103);
+$paragraph: rgb(95, 108, 123);
+$btn-text: rgb(255, 255, 254);
     
 *,
 *::after,
@@ -42,66 +67,151 @@
     padding: 0;
     box-sizing: border-box;
     text-decoration: none;
-}
-body {
-    width: 100vw;
-    height: 100vh;
-    background-color: #fffffe;
-}
+    font-family: 'Open Sans', sans-serif;
     
-header {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+}   
+.body {
+width: 100%;
+height: 100vh;
+background: $background;
 }
-header > img {
-  display: flex;
-  width: 130px;
-  height: auto;
-  margin: 15px;
-  cursor: pointer;
-}
-
-    
 .container {
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #fffffe;
+    width: 100%;
+    height: 85vh;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, .212);
+    
+}
+
+    
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 8rem;
+    
+}
+
+header > img {
+  display: flex;
+  width: 130px;
+  height: auto;
+  margin: 2rem;
+  cursor: pointer;
+  
+}
+
+.formulario {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.btn-cadastrar {
+background: $button;
+height: 2.5rem;
+width: 10rem;
+padding: 0.4rem 1rem;
+margin: 2rem;
+border-radius: 5px;
+line-height: 0;
+border: none;
+cursor: pointer;
 
 }
+a {
+    color: $btn-text;
+    font-weight: 800;
+    font-size: 0.9rem;
+}
+
+.btn-cadastrar:hover {
+    background: #3da9fc99;
     
+}
+
 .card-login {
-    padding: 6em;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: center;
-    padding: 30px 35px;
-    background: #fffffe;
-    border-radius: 20px;
+    border-radius: 1.2rem;
     box-shadow: 0px 10px 40px #00000056;
+    border: none;
+    padding: 5rem;
+    
     
 }
 
-.card-login > h2 {
-    color: #094067;
+
+.card-login h1::after {
+    content: '';
+    display: block;
+    width: 100% ;
+    height: 0.3rem;
+    background: $button;
+    margin-bottom: 2rem;
+    border-radius: 10px;
     font-weight: 800;
-    margin: 10px;
+    
 
 }
-
-.card-login > input {
-    width: 100%;
+.card-login input {
+    margin: 0.6rem 0;
+    padding: 0.8 1.2rem;
+    width: 20rem;
     padding: 15px;
+    border: none;
     border-radius: 12px;
     font-size: 12pt;
-    box-shadow: 0px 10px 40px #00000056;
+    box-shadow: 1px 1px 10px #0000001c;
     outline: none;
-    box-sizing: border-box;
+    
 }
 
-.card-login > label {
+.card-login input:hover {
+    background-color: #eeeeee99;
+}
+.card-login input:focus-visible {
+    outline: 1px solid $button;
+}
+
+  label {
+    font-weight: 500;
+    transition: all 0.1s ease;
 
 }
+input:focus ~ label,
+input:not(:focus):valid ~ label{
+    top: -15px;
+		bottom: 0px;
+		left: 0px;
+		font-size: 11px;
+		opacity: 1;
+		color: #404040;
+}
+
+.card-login button {
+    width: 100%;
+    border: none;
+    margin: 1.5rem 0;
+    background: $button;
+    padding: 0.62rem;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.card-login button:hover{
+    background: #3da9fc99;
+}
+
+.card-login button a {
+    font-size: 0.93rem;
+    font-weight: 500;
+}
+
 </style>
