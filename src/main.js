838f43d96abd0bc43216d8740ecbd931b068f718
charlesYS <charlesmendes6@gmail.com>
2022-10-10 18:12:02 -0300

    login validations with some issues, fixing

--- src/main.js
+++ src/main.js
@@ -3,5 +3,16 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import Toast from "vue-toastification";
+
+import "vue-toastification/dist/index.css";
 
 createApp(App).use(store).use(router).mount('#app');
+
+import Vuelidate from "vuelidate";
+Vue.use(Vuelidate)
+
+app.use(Toast, options);
+const options = {
+    
+};
