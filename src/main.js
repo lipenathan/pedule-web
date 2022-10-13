38bc9a3f8e642f88691fc841e5464ee6625c6651
FabioNote <fabioggoulart17@gmail.com>
2022-10-12 21:56:20 -0300

    feito pull

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import Vuelidate from 'vuelidate';
 
+Vue.use(Vuelidate)
 createApp(App).use(store).use(router).mount('#app');
