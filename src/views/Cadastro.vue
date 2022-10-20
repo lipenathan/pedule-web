ef781e141d15256ddf10744ae2f53a7f05da41fa
FabioGoulart17 <fabioggoulart17@gmail.com>
2022-10-20 18:58:04 -0300

    Ajuste de icones

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -10,9 +10,7 @@
         <div class="top-box">
           <div>
             <label for="name" id="lb-name">Nome </label>
-            <label for="name" id="icon-name"
-              ><i class="fa-solid fa-user"></i
-            ></label>
+            <span class="icon_name"><i class="fa-solid fa-user"></i></span>
             <input
               type="text"
               name="name"
@@ -20,10 +18,12 @@
               placeholder="Digite o seu nome"
               v-model="usuario.nome"
             />
+            
           </div>
 
           <div class="top-box-child">
-            <label for="birth">Data de nascimento</label>
+            <label id="lb_birth" for="birth">Data de nascimento</label>
+            <span class="icon_birth"><i class="fa-sharp fa-solid fa-cake-candles"></i></span>
             <input
               type="date"
               name="birth"
@@ -32,15 +32,16 @@
             />
           </div>
         </div>
-
+        
         <label for="institute">Instituição</label>
+        <span class="icon_institute"><i class="fa-sharp fa-solid fa-building-columns"></i></span>
         <input
           type="text"
           name="institute"
           id="inp-institute"
           placeholder="Informe a instituição"
-          v-model="usuario.instituicao"
-        />
+          v-model="usuario.instituicao" class="inp_top_box2"/>
+        
 
         <label for="email">Email</label>
         <input
@@ -48,32 +49,29 @@
           name="email"
           id="inp-email"
           placeholder="Insira o seu email"
-          v-model="usuario.email"
+          v-model="usuario.email" class="inp_top_box2"
         />
+        <span class="icon_email" ><i class="fa-solid fa-envelope"></i></span>
 
         <label for="password">Senha</label>
+        <span class="icon_password"><i class="fa-solid fa-lock"></i></span>
         <input
           type="password"
           name="password"
           id="inp-password"
           placeholder="Insira sua senha"
-          v-model="usuario.password.password"
+          v-model="usuario.password.password" class="inp_top_box2"
         />
-        <!-- <span v-if="v$.usuario.password.password.$error">
-          {{ v$.usuario.password.password.$errors[0].$message }}
-        </span> -->
 
         <label for="conf-password">Cofirme sua senha</label>
+        <span class="icon_conf_password"><i class="fa-solid fa-lock"></i></span>
         <input
           v-model="usuario.password.confirm"
           type="password"
           name="conf-password"
           id="conf-password"
-          placeholder="Confirme sua senha"
+          placeholder="Confirme sua senha" class="inp_top_box2"
         />
-        <!-- <span v-if="v$.usuario.password.confirm.$error">
-          {{ v$.usuario.password.confirm.$errors[0].$message }}
-        </span> -->
 
         <button id="bt" @click.prevent="submitForm">Enviar</button>
       </div>
@@ -88,45 +86,7 @@ import { required, sameAs, minLength, helpers } from "@vuelidate/validators";
 import { reactive, computed } from "vue";
 
 export default {
-  // setup() {
-  //   const state = reactive({
-  //     usuario: {
-  //       nome: "",
-  //       email: "",
-  //       dataNascimento: "",
-  //       instituicao: "",
-  //       password: {
-  //         password: "",
-  //         confirm: "",
-  //       },
-  //     },
-  //   });
-
-  // const customRequired = {
-  //   name: {
-  //     password: helpers.withMessage("Esse campo é obrigatório", required),
-  //   },
-  // };
-
-  // const rules = computed(() => {
-  //   return {
-  //     password: {
-  //       password: { customRequired, minLength: minLength(6) },
-  //       confirm: {
-  //         customRequired,
-  //         sameAs: sameAs(usuario.password.password),
-  //       },
-  //     },
-  //   };
-  // });
-
-  // const v$ = useValidate(rules, state);
-
-  // return {
-  //   state,
-  //   v$,
-  // };
-  // },
+  
   data() {
     return {
       usuario: {
@@ -143,7 +103,6 @@ export default {
   },
   methods: {
     submitForm() {
-      // this.v$.$validate();
       Api().post("/usuario/novo", {
         nome: this.usuario.nome,
         email: this.usuario.email,
@@ -180,14 +139,6 @@ body {
   font-family: "Open Sans", sans-serif;
 }
 
-#icon-name {
-  font-size: 15px;
-  position: absolute;
-  top: 214px;
-  left: 530px;
-  opacity: 5.6;
-}
-
 .container {
   display: flex;
   justify-content: center;
@@ -201,7 +152,7 @@ body {
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
-  padding: 2rem;
+  padding: 4rem;
   border-radius: 15px;
   width: 300px;
   height: 470px;
@@ -210,8 +161,16 @@ body {
 }
 
 h1 {
-  margin-top: 0px;
-  margin-bottom: 0px;
+  margin-top: -70px;
+  margin-bottom:0px;
+}
+
+h2{
+  margin-bottom: 10px;
+}
+
+label {
+  padding: 5px;
 }
 
 .title {
@@ -220,6 +179,12 @@ h1 {
   align-items: center;
   width: 100%;
   color: $button;
+}
+
+#inp-name{
+  padding: 0 1rem;
+  width: 120px;
+  height: 25px;
 }
 
 .top-box {
@@ -235,14 +200,16 @@ h1 {
 }
 
 .top-box-child input[type="date"] {
-  width: 155px;
+  width: 134px;
   height: 23px;
+
 }
 
 input {
-  width: 100%;
+  width: 103%;
+  padding-left: 1.5rem;
   border-radius: 8px;
-  height: 30px;
+  height: 28px;
   border: 0.5px solid rgb(104, 104, 104, 0.2);
   margin-bottom: 10px;
 }
@@ -254,18 +221,62 @@ input::placeholder {
 .top-box div input {
   width: 120px;
 }
-
-.form input[type="submit"] {
-  background-color: $button;
-  height: 50px;
-  width: 306px;
-  margin-top: 10px;
-  margin-bottom: 0px;
-  font-size: 18px;
-  font-weight: bold;
-  color: #ffffff;
-  border: none;
-  box-shadow: 0px 2px 3px #888;
-  cursor: pointer;
+ 
+.icon_institute{
+  position: absolute;
+  margin-top:-99px;
+  margin-left: 0.4rem;
+  margin-bottom: 0.7rem;
+  opacity: 0.3;
 }
+
+.icon_email{
+   position: absolute;
+   margin-left: 0.4rem;
+   margin-top:41px;
+   opacity: 0.3;
+ }
+
+ .icon_password{
+   position: absolute;
+   margin-left: 0.4rem;
+   margin-top: 185px;
+   opacity: 0.3;
+ }
+
+ .icon_conf_password{
+   position: absolute;
+   margin-left: 0.4rem;
+   margin-top: 334px;
+   opacity: 0.3;
+ }
+
+ .icon_name{
+   position: absolute;
+   margin-left: -3.3rem;
+   margin-top: 22px;
+   opacity: 0.3;
+ }
+
+ .icon_birth{
+   position: absolute;
+   margin-left: -9.6rem;
+   margin-top: 23px;
+   opacity: 0.3;
+ }
+
+  button{
+   background-color: $button ;
+   border: none;
+   width: 112%;
+   height: 38px;
+   border-radius: 10px;
+   margin-top: 20px;
+   margin-bottom: -30px;
+   font-weight: bold;
+   font-size: 20px;
+   color: #FFFFFF;
+   box-shadow: 0px 2px 3px #888;
+   cursor: pointer;
+  }
 </style>
