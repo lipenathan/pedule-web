dff73cd749d1d8bf6ec7fc7cea297f8a1ab2ce23
lipenathan <lipenathan_jem@hotmail.com>
2022-11-02 11:55:13 -0300

    inicio desenvolvimento listagem de matérias

---
+++ src/components/materias/MateriaItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="item" :style="{ background: materia.color }">
+    <h3>{{ materia.titulo }}</h3>
+    <i class="pi pi-pencil"></i>
+    <i class="pi pi-trash"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    materia: {
+      id: null,
+      titulo: "",
+      professor: "",
+      descricao: "",
+      cor: "",
+      usuario: {},
+      semanaHorario: [],
+    },
+  },
+};
+</script>
+
+<style scoped>
+.item {
+  display: flex;
+  width: 100%;
+  background-color: blue;
+  margin: 5px 0;
+  border-radius: 10px;
+  padding: 5px;
+}
+
+.pi-pencil {
+}
+
+.pi-trash {
+}
+</style>
--- src/views/Materias.vue
+++ src/views/Materias.vue
@@ -4,50 +4,73 @@
     <h1>Tela a ser Implementada</h1>
   </div>
   <div class="dialog">
-    <p-button @click="showDialog = !showDialog">Show Materia Dialog</p-button>
+    <!-- <p-button @click="showDialog = !showDialog">Show Materia Dialog</p-button> -->
     <dialog-materia
       :show="showDialog"
       @closedDialog="showDialog = false"
       :update="false"
       :materia="materia"
     ></dialog-materia>
+    <p-button
+      id="plus"
+      class="p-button-raised p-button-rounded button-add"
+      icon="pi pi-plus"
+      @click="showDialog = !showDialog"
+    />
+    <materia-item v-for="item in materias" :key="item.id" :materia="item"></materia-item>
   </div>
 </template>
 <script>
 import Navbar from "@/components/template/Navbar.vue";
 import DialogMateria from "@/components/materias/MateriaDialog.vue";
+import MateriaItem from '@/components/materias/MateriaItem.vue'
+
 import PButton from "primevue/button";
+import PDataTable from 'primevue/datatable'
+
+import api from '@/services/API'
 
 export default {
-  components: { Navbar, DialogMateria, PButton },
+  components: { Navbar, DialogMateria, PButton, PDataTable, MateriaItem },
   data() {
     return {
       showDialog: false,
-      /**
-       *exemplo de objeto de matéria que na verdade deverá ser pego da lista
-       *que está sendo exibida ao usuário e que foi recebida via api
-       */
-      materia: {
-        titulo: "Materia Exemplo",
-        descricao: "matéria de teste passada de um componente pai para um componente filho",
-        semanaHorario: [
-          {
-            horario: "20:15:00",
-            semana: {
-              id: 2,
-            },
-          },
-        ],
-        usuario: {
-          id: 15,
-        },
+      usuario: {
+        id: 16
       },
+      materias: [{
+        id: null,
+        titulo: "",
+        professor: "",
+        descricao: "",
+        cor: "",
+        usuario: {},
+        semanaHorario: []
+      }]
     };
   },
+  methods: {
+    buscarMaterias() {
+      api().get(`/materia/listar/${this.usuario.id}`).then((res) => {
+        this.materias = res.data
+        console.log(this.materias)
+      })
+    }
+  },
+  created() {
+    this.buscarMaterias()
+  }
 };
 </script>
 <style lang="scss" scoped>
 .dialog {
   margin-top: 4rem;
+}
+
+.button-add{
+  width: 3.5rem!important;
+  height: 3.5rem!important;
+  position: absolute;
+
 }
 </style>
