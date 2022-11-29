c74e12d85af5126326861e0464187739034839f5
FabioGoulart17 <fabioggoulart17@gmail.com>
2022-11-29 20:26:09 -0300

    commit para teste

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -1,87 +1,44 @@
 <template>
   <body>
     <Header />
-    <main class="container">
-      <div class="form">
-        <div class="title">
-          <h1>Bem vindo!</h1>
-        </div>
-
-        <h2>Cadastre-se</h2>
-        <div class="top-box">
-           <div class="field col-12 md:col-4">
-                    <span class="p-float-label p-input-icon-left">
-                        <i class="pi pi-search" />
-                        <InputText id="inputtext-left" type="text" v-model="valueIconLeft" />
-                        <label for="inputtext-left">Left Icon</label>
-                    </span>
-                </div>
-            
-            <!-- <input
-              type="text"
-              name="name"
-              id="inp-name"
-              placeholder="Digite o seu nome"
-              v-model="usuario.nome" required
-            /> -->
-            
-          
-
-          <div class="top-box-child">
-            <label id="lb_birth" for="birth">Data de nascimento</label>
-            <span class="icon_birth"><i class="fa-sharp fa-solid fa-cake-candles"></i></span>
-            <input
-              type="date"
-              name="birth"
-              id="birth"
-              v-model="usuario.dataNascimento" required
-            />
-          </div>
-        </div>
-        
-        <label for="institute">Instituição</label>
-        <span class="icon_institute"><i class="fa-sharp fa-solid fa-building-columns"></i></span>
-        <input
-          type="text"
-          name="institute"
-          id="inp-institute"
-          placeholder="Informe a instituição"
-          v-model="usuario.instituicao" class="inp_top_box2" required/>
-        
-
-        <label for="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="inp-email"
-          placeholder="Insira o seu email"
-          v-model="usuario.email" class="inp_top_box2" required
-        />
-        <span class="icon_email" ><i class="fa-solid fa-envelope"></i></span>
-
-        <label for="password">Senha</label>
-        <span class="icon_password"><i class="fa-solid fa-lock"></i></span>
-        <input
-          type="password"
-          name="password"
-          id="inp-password"
-          placeholder="Insira sua senha"
-          v-model="usuario.password.password" class="inp_top_box2" required
-        />
-
-        <label for="conf-password">Cofirme sua senha</label>
-        <span class="icon_conf_password"><i class="fa-solid fa-lock"></i></span>
-        <input
-          v-model="usuario.password.confirm"
-          type="password"
-          name="conf-password"
-          id="conf-password"
-          placeholder="Confirme sua senha" class="inp_top_box2" required
-        />
-
-        <button id="bt" @click.prevent="submitForm">Enviar</button>
+    <div class="form">
+      <div class="title">
+        <h1>Bem vindo!</h1>
       </div>
-    </main>
+
+      <h2>Cadastre-se</h2>
+      <div class="name_date">
+        <div class="name_user">
+          <span class="p-float-label">
+            <PInputText id="username" type="text" v-model="value" />
+            <label for="username">Username</label>
+          </span>
+        </div>
+        <div class="inputtext3">
+          <span class="p-float-label p-input-icon-left">
+            <!-- <i class="pi-calendar"></i> -->
+            <p-calendar
+              dateFormat="dd/mm/yy"
+              id="inputtext-left2"
+              v-model="dataForm"
+            />
+            <label for="" id="lb_icon_calendar"
+              ><i class="fa-solid fa-calendar-days"></i
+            ></label>
+            <label for="inputtext-left">Data de nascimento</label>
+          </span>
+          <!-- <small
+            v-if="
+              (v$.dataForm.$invalid && submitted) ||
+              v$.dataForm.$pending.$response
+            "
+            class="p-error"
+            >{{ dataEntregaRequired }}</small
+          > -->
+        </div>
+      </div>
+      <button id="bt" @click.prevent="submitForm">Enviar</button>
+    </div>
   </body>
 </template>
 
@@ -89,16 +46,18 @@
 import Api from "../services/API";
 import Header from "@/components/template/Header.vue";
 import PInputText from "primevue/inputtext";
+import PCalendar from "primevue/calendar";
 import useValidate from "@vuelidate/core";
 import { required, sameAs, minLength, helpers } from "@vuelidate/validators";
 import { reactive, computed } from "vue";
 
 export default {
-
   components: {
-     Header, PInputText,
+    Header,
+    PInputText,
+    PCalendar,
   },
-  
+
   data() {
     return {
       usuario: {
@@ -106,29 +65,30 @@ export default {
         email: "",
         dataNascimento: "",
         instituicao: "",
-          password: "",
-          confirm: "",
-      
+        password: "",
+        confirm: "",
       },
     };
   },
   methods: {
     submitForm() {
-      Api().post("/usuario/novo", {
-        nome: this.usuario.nome,
-        email: this.usuario.email,
-        dataNascimento: this.usuario.dataNascimento,
-        instituicao: this.usuario.instituicao,
-        senha: this.usuario.password.password,
-      }).then(res => {
-        this.usuario.nome = ""
-        this.usuario.email = ""
-        this.usuario.dataNascimento = null
-        this.usuario.instituicao = ""
-        this.usuario.password.password = ""
-        this.usuario.password.confirm = ""
-        this.$router.push('/login')
-      })
+      Api()
+        .post("/usuario/novo", {
+          nome: this.usuario.nome,
+          email: this.usuario.email,
+          dataNascimento: this.usuario.dataNascimento,
+          instituicao: this.usuario.instituicao,
+          senha: this.usuario.password.password,
+        })
+        .then((res) => {
+          this.usuario.nome = "";
+          this.usuario.email = "";
+          this.usuario.dataNascimento = null;
+          this.usuario.instituicao = "";
+          this.usuario.password.password = "";
+          this.usuario.password.confirm = "";
+          this.$router.push("/login");
+        });
     },
   },
 };
@@ -148,16 +108,8 @@ body {
   padding: 0;
   box-sizing: border-box;
   text-decoration: none;
-  font-family: "Open Sans", sans-serif; 
+  font-family: "Open Sans", sans-serif;
 }
-
-// .container {
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   height: 100vh;
-//   width: 100%;
-// }
 
 .form {
   display: flex;
@@ -171,22 +123,17 @@ body {
   background-color: #f5f5f5;
   box-shadow: 2px 3px 5px #888;
   margin: 0 auto;
-  
 }
 
 h1 {
   margin-top: -50px;
-  margin-bottom:0px;
+  margin-bottom: 0px;
   font-size: 30px;
 }
 
-h2{
+h2 {
   margin-bottom: 10px;
-  font-size: 20px
-}
-
-label {
-  padding: 5px;
+  font-size: 20px;
 }
 
 .title {
@@ -197,102 +144,26 @@ label {
   color: $button;
 }
 
-#inp-name{
-  padding: 0 1rem;
-  width: 120px;
-  height: 25px;
+.name_date{
+  display:flex
 }
 
-.top-box {
-  display: flex;
-  text-align: start;
-  width: 306px;
-}
-.top-box div input[type="text"] {
-  height: 23px;
-}
-.top-box-child {
-  margin-left: 21px;
-}
-
-.top-box-child input[type="date"] {
-  width: 134px;
-  height: 23px;
-
-}
-
-input {
-  width: 103%;
-  padding-left: 1.5rem;
+#username{
   border-radius: 8px;
-  height: 28px;
-  border: 0.5px solid rgb(104, 104, 104, 0.2);
-  margin-bottom: 10px;
 }
 
-input::placeholder {
-  color: #ccc;
+button {
+  background-color: $button;
+  border: none;
+  width: 110%;
+  height: 38px;
+  border-radius: 10px;
+  margin-top: 20px;
+  margin-bottom: -30px;
+  font-weight: bold;
+  font-size: 20px;
+  color: #ffffff;
+  box-shadow: 0px 2px 3px #888;
+  cursor: pointer;
 }
-
-.top-box div input {
-  width: 120px;
-}
- 
-.icon_institute{
-  position: absolute;
-  margin-top:-99px;
-  margin-left: 0.4rem;
-  margin-bottom: 0.7rem;
-  opacity: 0.3;
-}
-
-.icon_email{
-   position: absolute;
-   margin-left: 0.4rem;
-   margin-top:41px;
-   opacity: 0.3;
- }
-
- .icon_password{
-   position: absolute;
-   margin-left: 0.4rem;
-   margin-top: 185px;
-   opacity: 0.3;
- }
-
- .icon_conf_password{
-   position: absolute;
-   margin-left: 0.4rem;
-   margin-top: 334px;
-   opacity: 0.3;
- }
-
- .icon_name{
-   position: absolute;
-   margin-left: -3.3rem;
-   margin-top: 22px;
-   opacity: 0.3;
- }
-
- .icon_birth{
-   position: absolute;
-   margin-left: -9.6rem;
-   margin-top: 23px;
-   opacity: 0.3;
- }
-
-  button{
-   background-color: $button ;
-   border: none;
-   width: 110%;
-   height: 38px;
-   border-radius: 10px;
-   margin-top: 20px;
-   margin-bottom: -30px;
-   font-weight: bold;
-   font-size: 20px;
-   color: #FFFFFF;
-   box-shadow: 0px 2px 3px #888;
-   cursor: pointer;
-  }
 </style>
